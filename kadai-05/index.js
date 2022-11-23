2865e1e63ba2764915570227853ba4a5a3072b90
ItsukiMatsuo <itsukimatsuo0@gmail.com>
2022-11-23 05:10:06 +0000

    課題5を提出

--- kadai-05/index.js
+++ kadai-05/index.js
@@ -1,13 +1,19 @@
 const profile = {
-    name: 'matsuo',
+    name: "松尾",
     age: 19,
-    hobby: '鉄道写真撮影',
-    major: '日本史'
+    hobby: "鉄道写真撮影",
+    major: "日本史学",
+    birthplace: "福井",
    }
 
-const dictionary = {}
+const dictionary = {
+    name: "名前",
+    age: "年齢",
+    hobby: "趣味",
+    major: "専攻",
+    birthplace: "出身地",
+}
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (const key of Object.keys(profile)) {
+    console.log("私の" + dictionary[key] + "は" + profile[key] + "です")
 }
