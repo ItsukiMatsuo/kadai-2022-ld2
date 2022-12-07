0aeff81eac35db2044903217d969081fd938139b
ItsukiMatsuo <itsukimatsuo0@gmail.com>
2022-12-07 05:42:29 +0000

    課題6を提出

--- kadai-06/index.js
+++ kadai-06/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
-const json = fs.readFileSync('./kadai-o6/data.json').toString()
+const json = fs.readFileSync('./kadai-06/data.json').toString()
 
 let object
 try {
-    const object = JSON.parse(json)
+    object = JSON.parse(json)
 } catch (error) {
     console.log('不正なJSONフォーマットです')
     process.exit(1)
@@ -13,5 +13,5 @@ object.count = object.count + 1
 object.updateAt = new Date().toISOString()
 fs.writeFileSync(
     './kadai-06/data.json',
-    JSON.stringify(object)
+    JSON.stringify(object, null, 2)
 )
